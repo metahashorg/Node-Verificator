8ae6fdafdf6b53cf33e74ca66cfdee277a5afa4d
Dmitriy Borisenko <otselnik@yandex.ru>
2020-06-08 12:20:08 +0400

    update libraries from core

--- metalibs/meta_log/src/meta_log.cpp
+++ metalibs/meta_log/src/meta_log.cpp
@@ -7,13 +7,14 @@
 
 #include "date.h"
 
-moodycamel::ConcurrentQueue<std::stringstream*>* __output_queue = new moodycamel::ConcurrentQueue<std::stringstream*>();
-std::thread* __cout_printer = new std::thread([]() {
-    moodycamel::ConsumerToken ct(*__output_queue);
+namespace meta_log {
+moodycamel::ConcurrentQueue<std::stringstream*>* output_queue = new moodycamel::ConcurrentQueue<std::stringstream*>();
+std::thread* cout_printer = new std::thread([]() {
+    moodycamel::ConsumerToken ct(*output_queue);
 
-    while (true) {
+    for (;;) {
         std::stringstream* p_ssout;
-        if (__output_queue->try_dequeue(ct, p_ssout)) {
+        if (output_queue->try_dequeue(ct, p_ssout)) {
 
             std::cout << p_ssout->rdbuf() << std::endl;
 
@@ -24,7 +25,8 @@ std::thread* __cout_printer = new std::thread([]() {
     }
 });
 
-void print_to_stream_date_and_place(std::stringstream* p_ss, const std::string& file, const std::string& function, const int line)
+void print_to_stream_date_and_place(std::stringstream* p_ss, const std::string& file, const std::string& function,
+    const int line)
 {
     const auto found = file.find_last_of("/\\");
     const auto filename = (found == std::string::npos) ? file : file.substr(found + 1);
@@ -32,13 +34,16 @@ void print_to_stream_date_and_place(std::stringstream* p_ss, const std::string& 
     auto tp = std::chrono::system_clock::now();
     auto dp = date::floor<date::days>(tp);
 
-    auto ymd = date::year_month_day{ dp };
+    auto ymd = date::year_month_day { dp };
     auto time = date::make_time(std::chrono::duration_cast<std::chrono::milliseconds>(tp - dp));
+
+    uint64_t micro = std::chrono::duration_cast<std::chrono::microseconds>(tp - dp).count() % 1000000l;
 
     (*p_ss) << ymd.year() << "/" << ymd.month() << "/" << ymd.day() << " "
             << std::setfill(' ') << std::setw(2) << time.hours().count() << ":"
             << std::setfill('0') << std::setw(2) << time.minutes().count() << ":"
             << std::setfill('0') << std::setw(2) << time.seconds().count() << ":"
-            << std::setfill('0') << std::setw(3) << time.subseconds().count() << " "
+            << std::setfill('0') << std::setw(6) << micro << " "
             << filename << ":" << line << ":" << function << "$\t";
+}
 }
--- metalibs/thread_pool/src/thread_pool.cpp
+++ metalibs/thread_pool/src/thread_pool.cpp
@@ -1,21 +1,15 @@
-#include <cstring>
-#include <meta_log.hpp>
 #include <thread_pool.hpp>
 
-void ThreadPool::set_timer(uint64_t timeout_ms, ThreadPool::fn_type* task)
+std::tuple<std::vector<boost::thread>, boost::asio::io_context::work> thread_pool(boost::asio::io_context& io_context, uint64_t thread_count)
 {
-    uint64_t stop = timeout_ms + static_cast<uint64_t>(std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::system_clock::now()).time_since_epoch().count());
+    std::vector<boost::thread> threadpool;
+    boost::asio::io_context::work work(io_context);
 
-    std::thread([this, stop, task]() {
-        while (true) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
-            uint64_t time_ms = static_cast<uint64_t>(std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::system_clock::now()).time_since_epoch().count());
+    for (uint i = 0; i < thread_count; i++) {
+        threadpool.emplace_back([&io_context]() {
+                io_context.run();
+            });
+    }
 
-            if (time_ms >= stop) {
-                runTask(task);
-                return;
-            }
-        }
-    })
-        .detach();
+    return { std::move(threadpool), std::move(work) };
 }
