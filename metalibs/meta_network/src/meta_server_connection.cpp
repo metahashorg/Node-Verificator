eb89688846e5e0df0580c4d9be5a00627bfb5ec5
Dmitriy Borisenko <otselnik@yandex.ru>
2021-03-08 17:29:17 +0300

    sync metalibs

--- metalibs/meta_network/src/meta_server_connection.cpp
+++ metalibs/meta_network/src/meta_server_connection.cpp
@@ -1,13 +1,14 @@
 #include <meta_common.h>
-#include <meta_log.hpp>
+//#include <meta_log.hpp>
 #include <meta_server.h>
+#include <version.h>
 
 namespace metahash::network {
 
 namespace statics {
     std::vector<char> version_info(const std::string& mh_addr)
     {
-        static const std::string version;// = std::string(VERSION_MAJOR) + "." + std::string(VERSION_MINOR) + "." + std::string(GIT_COUNT);
+        static const std::string version = std::string(VERSION_MAJOR) + "." + std::string(VERSION_MINOR) + "." + std::string(GIT_COUNT);
         std::string resp_data = "{\"result\":{\"version\":\"" + version + "\",\"mh_addr\": \"" + mh_addr + "\"}}";
         static const std::string http_head = "HTTP/1.1 200 OK\r\n"
                                              "Version: HTTP/1.1\r\n"
@@ -63,24 +64,24 @@ void Connection::read(std::shared_ptr<Connection> pThis)
                 pThis->read(pThis);
             } break;
             case statics::WRONG_MAGIC_NUMBER: {
-                DEBUG_COUT("WRONG_MAGIC_NUMBER");
+                // DEBUG_COUT("WRONG_MAGIC_NUMBER");
                 pThis->reply.make_http(statics::version_info(pThis->signer.get_mh_addr()));
                 pThis->write_and_close(pThis);
             } break;
             case statics::UNKNOWN_SENDER_METAHASH_ADDRESS: {
-                DEBUG_COUT("UNKNOWN_SENDER_METAHASH_ADDRESS");
+                // DEBUG_COUT("UNKNOWN_SENDER_METAHASH_ADDRESS");
                 pThis->reply.make(pThis->signer, pThis->request.request_id, statics::unknown_sender);
                 pThis->write_and_close(pThis);
             } break;
             case statics::INVALID_SIGN: {
-                DEBUG_COUT("INVALID_SIGN");
+                // DEBUG_COUT("INVALID_SIGN");
                 pThis->reply.make(pThis->signer, pThis->request.request_id, statics::invalid_sign);
                 pThis->write_and_close(pThis);
             } break;
             }
         } else {
-            DEBUG_COUT("error");
-            DEBUG_COUT(e.message());
+            //DEBUG_COUT("error");
+            //DEBUG_COUT(e.message());
         }
     });
 }
